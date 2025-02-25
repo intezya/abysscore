29f406176b875f5c49b3b29a3430ffcbb788d2d0
Tim <intezya@gmail.com>
2025-02-25 16:20:55 +0400

    :tada: init abysscore

---
+++ src/main/kotlin/com/intezya/abysscore/AbyssCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.intezya.abysscore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AbyssCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<AbyssCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/intezya/abysscore/AbyssCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.intezya.abysscore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AbyssCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
