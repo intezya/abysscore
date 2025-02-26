6492c385750ea967c7d325d6ec77ddc99126ccf3
Tim <intezya@gmail.com>
2025-02-26 19:32:53 +0400

    :card_file_box: find by lower username

--- src/main/kotlin/com/intezya/abysscore/repository/UserRepository.kt
+++ src/main/kotlin/com/intezya/abysscore/repository/UserRepository.kt
@@ -2,10 +2,12 @@ package com.intezya.abysscore.repository
 
 import com.intezya.abysscore.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE LOWER(u.username) = :username")
     fun findByUsername(username: String): Optional<User>
 }
