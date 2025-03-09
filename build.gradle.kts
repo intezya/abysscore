d2f200467e3027b17185f814cec42e010e7ac888
Tim <intezya@gmail.com>
2025-03-09 09:46:24 +0400

    :pushpin: update dependencies sort, remove unused

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.1.10"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.autonomousapps.dependency-analysis") version "2.10.1"
 }
 
 group = "com.intezya"
@@ -19,50 +20,63 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot dependencies
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.projectlombok:lombok")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    runtimeOnly("org.postgresql:postgresql")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
-    implementation("org.liquibase:liquibase-core:4.23.0")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-    implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
-    implementation("de.mkammerer:argon2-jvm:2.11")
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+    // Kotlin dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("io.mockk:mockk:1.13.8")
-    testImplementation("com.ninja-squad:springmockk:4.0.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Jackson dependencies
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // JWT dependencies
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    // Liquibase and BouncyCastle dependencies
+    runtimeOnly("org.liquibase:liquibase-core:4.23.0")
+    runtimeOnly("org.bouncycastle:bcprov-jdk15on:1.70")
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
+
+    // Security & Authentication dependencies
+    implementation("de.mkammerer:argon2-jvm:2.11")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.security:spring-security-test")
+
+    // Database dependencies
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("com.zaxxer:HikariCP:5.1.0")
-    testImplementation("org.testcontainers:junit-jupiter:1.19.5")
     testImplementation("org.testcontainers:postgresql:1.19.5")
-    testImplementation("org.testcontainers:testcontainers:1.19.5")
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.mockito:mockito-core")
-    testImplementation("org.mockito:mockito-junit-jupiter")
-    implementation("io.github.serpro69:kotlin-faker:1.16.0")
+
+    // Swagger/OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+
+    // Test dependencies
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.5")
+    testRuntimeOnly("org.testcontainers:testcontainers:1.19.5")
+    testImplementation("io.github.serpro69:kotlin-faker:1.16.0")
     testImplementation("io.rest-assured:rest-assured:5.3.0")
     testImplementation("io.rest-assured:kotlin-extensions:5.3.0")
     testImplementation("io.rest-assured:json-path:5.3.0")
     testImplementation("io.rest-assured:xml-path:5.3.0")
+
+    // Lombok
+    runtimeOnly("org.projectlombok:lombok")
 }
+
 
 kotlin {
     compilerOptions {
