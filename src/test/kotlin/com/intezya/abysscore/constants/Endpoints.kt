7e440c8d2f40d1b271a1332a01e5af9f97bc1e95
Tim <intezya@gmail.com>
2025-03-17 17:31:10 +0400

    :white_check_mark: add user item tests

--- src/test/kotlin/com/intezya/abysscore/constants/Endpoints.kt
+++ src/test/kotlin/com/intezya/abysscore/constants/Endpoints.kt
@@ -7,3 +7,4 @@ const val INFO_PATH = "$AUTH_BASE_PATH/info"
 
 const val MATCH_INVITES_ENDPOINT = "/match/invites"
 const val USER_PREFERENCES_INVITES_ENDPOINT = "/users/preferences/invites"
+const val ACCOUNT_INVENTORY_ENDPOINT = "/account/inventory"
